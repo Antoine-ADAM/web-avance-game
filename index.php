6e7fdb4fbb156ce8bd3df2d08b31606b296b0c0d
Antoine ADAM <flancraft10@gmail.com>
2022-09-15 15:44:28 +0200

    dev index (router)

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
 <?php
+require_once 'controllers.php';
+require_once 'models/Pages.php';
 
+init();
+switch ($_GET['page']){
+    case Pages::HOME:
+        home();
+        break;
+    case Pages::GAME:
+        game();
+        break;
+    case Pages::LOGIN:
+        login();
+        break;
+    case Pages::REGISTER:
+        createAccount();
+        break;
+    case Pages::LOGOUT:
+        logout();
+        break;
+    case Pages::PURCHASE:
+        purchase();
+        break;
+    case Pages::ATTACK:
+        attack();
+        break;
+    default:
+        Pages::redirect(Pages::HOME);
+        break;
+}
