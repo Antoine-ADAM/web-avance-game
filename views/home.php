e4196218ccb8c03a33ff444577c8afed74c61354
Antoine ADAM <flancraft10@gmail.com>
2022-09-20 00:44:54 +0200

    DEBUG DEV 16/09 9

--- views/home.php
+++ views/home.php
@@ -5,6 +5,7 @@
 <body>
     <h1>Home</h1>
     <p>Home page content</p>
+    <h3 style="color: red">Ne pas oublier de préciser qu'on a une surcouche de MYSQLI pour la fonction query (S'il ne regarde pas en détail, il va nous enlever des points pour la sécu)</h3>
     <form action="<?= Pages::toURL(Pages::LOGIN) ?>" method="post">
         <input type="text" name="name" placeholder="name">
         <input type="password" name="password" placeholder="password">
